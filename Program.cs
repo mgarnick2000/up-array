94130543e81813c276cb080a5802581520dd3cfe
Mark <mgarnick@techtonicgroup.com>
2019-09-23 11:53:49 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+namespace up_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = new int[] { 2, 3, 9 };
+            var newNum = new int[] { 2, 4, 0 };
+            UpArray(num);
+        }
+
+        static int[] UpArray(int[] num) {
+        
+            return num;
+
+        }
+
+    }
+}
